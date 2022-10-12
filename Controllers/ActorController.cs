3cb0a3a9bb1911a8b8406f542bdffa776b2afa74
Nothung429 <frank.gatto96.fg@gmail.com>
2022-10-12 17:53:40 +0200

    add features, actors and genres CRUD

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Controllers
+{
+    public class ActorController : Controller
+    {
+        NetflixContext context = new NetflixContext();
+
+        // GET: Actor
+        public ActionResult Index()
+        {
+            return View(context.Actors.ToList());
+        }
+
+        // GET: Actor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || context.Actors == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await context.Actors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // GET: Actor/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actor/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Surname,Nationality")] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(actor);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actor/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || context.Actors == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await context.Actors.FindAsync(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: Actor/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Name,Surname,Nationality")] Actor actor)
+        {
+            if (id != actor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(actor);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActorExists(actor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actor/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || context.Actors == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await context.Actors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // POST: Actor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (context.Actors == null)
+            {
+                return Problem("Entity set 'NetflixContext.Actors'  is null.");
+            }
+            var actor = await context.Actors.FindAsync(id);
+            if (actor != null)
+            {
+                context.Actors.Remove(actor);
+            }
+            
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActorExists(int? id)
+        {
+          return context.Actors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/CharacteristicController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_boolflix.Controllers
+{
+    public class CharacteristicController : Controller
+    {
+        NetflixContext context = new NetflixContext();
+
+        // GET: Characteristic
+        public ActionResult Index()
+        {
+              return View(context.Characteristics.ToList());
+        }
+
+        // GET: Characteristic/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || context.Characteristics == null)
+            {
+                return NotFound();
+            }
+
+            var characteristic = await context.Characteristics.FirstOrDefaultAsync(m => m.Id == id);
+            if (characteristic == null)
+            {
+                return NotFound();
+            }
+
+            return View(characteristic);
+        }
+
+        // GET: Characteristic/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Characteristic/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Characteristic characteristic)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(characteristic);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(characteristic);
+        }
+
+        // GET: Characteristic/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || context.Characteristics == null)
+            {
+                return NotFound();
+            }
+
+            var characteristic = await context.Characteristics.FindAsync(id);
+            if (characteristic == null)
+            {
+                return NotFound();
+            }
+            return View(characteristic);
+        }
+
+        // POST: Characteristic/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Name")] Characteristic characteristic)
+        {
+            if (id != characteristic.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(characteristic);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CharacteristicExists(characteristic.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(characteristic);
+        }
+
+        // GET: Characteristic/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || context.Characteristics == null)
+            {
+                return NotFound();
+            }
+
+            var characteristic = await context.Characteristics
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (characteristic == null)
+            {
+                return NotFound();
+            }
+
+            return View(characteristic);
+        }
+
+        // POST: Characteristic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (context.Characteristics == null)
+            {
+                return Problem("Entity set 'NetflixContext.Characteristics'  is null.");
+            }
+            var characteristic = await context.Characteristics.FindAsync(id);
+            if (characteristic != null)
+            {
+                context.Characteristics.Remove(characteristic);
+            }
+            
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CharacteristicExists(int? id)
+        {
+          return context.Characteristics.Any(e => e.Id == id);
+        }
+    }
+}
